1bc499dd337d5b8b9f6a79a0ef202bf49328d27f
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2020-12-09 11:37:03 +0300

    feat: add new card popup functionality

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let popupNode = document.querySelector('.popup');
 let profileEditBtnNode = document.querySelector('.profile__edit-btn');
+let profileAddBtnNode = document.querySelector('.profile__add-btn');
 let profileNameNode = document.querySelector('.profile__name');
 let profileStatusNode = document.querySelector('.profile__status');
-let popupInputNameNode  = document.querySelector('.popup__input_type_name');
-let popupInputStatusNode  = document.querySelector('.popup__input_type_status');
+let popupTitleNode = document.querySelector('.popup__title');
+let popupInputMainNode  = document.querySelector('.popup__input_type_main');
+let popupInputContentNode  = document.querySelector('.popup__input_type_content');
 let popupCloseBtnNode = document.querySelector('.popup__close-btn');
 let popupSubmitBtnNode = document.querySelector('.popup__submit-btn');
 
@@ -42,34 +44,71 @@ const initialCards = [
   }
 ];
 
-function togglePopup (event) {
+function openPopup (event) {
   event.preventDefault();
-  popupNode.classList.toggle('popup_visible');
+  if ([...event.target.classList].includes('btn_type_edit')){
+    popupTitleNode.textContent = 'Редактировать профиль';
+    changeInputText();
+    popupInputMainNode.placeholder = 'Редактировать профиль';
+    popupInputContentNode.type = 'text';
+    popupInputContentNode.placeholder = 'Статус';
+    popupSubmitBtnNode.classList.remove('popup__add-btn');
+  } else {
+    popupTitleNode.textContent = 'Новое место';
+    popupInputMainNode.placeholder = 'Название';
+    popupInputContentNode.placeholder = 'Ссылка на картинку';
+    popupInputMainNode.value = '';
+    popupInputContentNode.type = 'url';
+    popupInputContentNode.value = '';
+    popupSubmitBtnNode.classList.add('popup__add-btn');
+  }
+  popupNode.classList.add('popup_visible');
+}
+
+function closePopup (event) {
+  popupNode.classList.remove('popup_visible');
 }
 
 function changeInputText() {
-  popupInputNameNode.value = profileNameNode.textContent;
-  popupInputStatusNode.value = profileStatusNode.textContent;
+  popupInputMainNode.value = profileNameNode.textContent;
+  popupInputContentNode.value = profileStatusNode.textContent;
 }
 
-function changeProfileInfo (event) {
-  event.preventDefault();
-  profileNameNode.textContent = popupInputNameNode.value;
-  profileStatusNode.textContent = popupInputStatusNode.value;
-  togglePopup(event);
-}
-
-popupCloseBtnNode.addEventListener('click', togglePopup);
-
-profileEditBtnNode.addEventListener('click', togglePopup);
-profileEditBtnNode.addEventListener('click', changeInputText);
-
-popupSubmitBtnNode.addEventListener('click', changeProfileInfo);
-
-initialCards.map(elem => {
+function makeNewCard(cardData) {
   let card = templateCard.cloneNode(true);
-  card.querySelector('.card__img').src = elem.link;
-  card.querySelector('.card__img').alt = elem.alt;
-  card.querySelector('.card__title').textContent = elem.name;
+  card.querySelector('.card__img').src = cardData.link;
+  card.querySelector('.card__img').alt = cardData.alt;
+  card.querySelector('.card__title').textContent = cardData.name;
+  return card;
+}
+
+function submitPopup (event) {
+  event.preventDefault();
+  if ([...event.target.classList].includes('popup__add-btn')) {
+    let cardData =
+      {
+        name: popupInputMainNode.value,
+        link: popupInputContentNode.value,
+        alt: popupInputMainNode.value
+      };
+    let card = makeNewCard(cardData);
+    placesGridNode.prepend(card)
+  } else {
+    profileNameNode.textContent = popupInputMainNode.value;
+    profileStatusNode.textContent = popupInputContentNode.value;
+  }
+  closePopup(event);
+}
+
+popupCloseBtnNode.addEventListener('click', closePopup);
+
+profileEditBtnNode.addEventListener('click', openPopup);
+
+profileAddBtnNode.addEventListener('click', openPopup);
+
+popupSubmitBtnNode.addEventListener('click', submitPopup);
+
+initialCards.forEach(elem => {
+  let card = makeNewCard(elem);
   placesGridNode.append(card);
 })
