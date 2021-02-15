dcab85701ead1350e44f23b2fcebc8634334eb3b
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-02-15 18:05:26 +0300

    ser devtool source map

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         publicPath: ''
   },
     mode: 'development',
+    devtool: 'inline-source-map',
   devServer: {
     contentBase: path.resolve(__dirname, './dist'),
     compress: true,
